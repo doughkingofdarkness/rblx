571e504a9a2502146be5ff169570afc9480557bb
kaisson <redhatsad1@gmail.com>
2026-05-31 10:13:38 +0300

    Delete W-azure directory

--- W-azure/AnimeVanguard/OneClickLoader.lua
+++
@@ -1,4 +0,0 @@
-if getgenv().AnimeVanguardLoaded then
-  return
-end
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c79108b9bdab949ccf29f7837a4408fd.lua"))()
--- W-azure/BloxFruit/OneClickLoader.lua
+++
@@ -1,4 +0,0 @@
-if getgenv().Loaded then
-  return 
-end
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/578f59fee3db574426301395ebf89069.lua"))()
